6f015c1af5f077837d289f60d7b690d7770f3cf0
mtrontz-the-dev <mtrontz@trontzenterprises.com>
2021-09-27 08:22:46 -0400

    added users route

---
+++ config.js
@@ -0,0 +1,10 @@
+// Architect extension for template file generation
+
+module.exports = {
+    name: true,
+    paths: [
+      "ARCHITECT_NAME/js/index.js",
+      "ARCHITECT_NAME/index.html",
+      "ARCHITECT_NAME/css/styles.css"
+    ]
+  };
